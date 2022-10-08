c0e2bcf5673c73cdb6f9f85ecf41b148092c3c1a
miahbates <miahbates@icloud.com>
2022-10-08 13:21:49 +0100

    fix(login) new design

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -5,31 +5,21 @@ import useStyles from "./styles";
 
 const LandingPage: React.FC = () => {
   const { classes } = useStyles();
-  const router = useRouter();
 
   return (
-    <div className={classes.wrapper}>
-      <Group className={classes.flexColumn}>
-        <Image
-          src="/pubclublogo.png"
-          alt="pub club logo"
-          width={250}
-          height={250}
-          className={classes.logo}
-        ></Image>
-        <Group mt="lg">
-          <Button
-            component="a"
-            href="/api/auth/login?returnTo=/api/login"
-            variant="gradient"
-            gradient={{ from: "teal", to: "blue", deg: 60 }}
-            className={classes.login}
-          >
-            Login / Register
-          </Button>
-        </Group>
-      </Group>
-    </div>
+    <Group className={classes.wrapper}>
+      <div className={classes.loginWrapper}>
+        <Button
+          component="a"
+          href="/api/auth/login?returnTo=/home"
+          variant="gradient"
+          gradient={{ from: "teal", to: "blue", deg: 60 }}
+          className={classes.login}
+        >
+          Login / Register
+        </Button>
+      </div>
+    </Group>
   );
 };
 
--- src/components/LandingPage/styles.ts
+++ src/components/LandingPage/styles.ts
@@ -2,42 +2,23 @@ import { withTheme } from "@emotion/react";
 import { createStyles } from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
-  logo: {
-    borderRadius: "5px",
-    padding: theme.spacing.lg,
-  },
-  flexColumn: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignContent: "center",
-    borderRadius: "5px",
-    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-    padding: theme.spacing.lg,
-    background: "white",
-  },
   login: {
     textDecoration: "none",
     width: "150px",
   },
-  register: {
-    textDecoration: "none",
-    width: "100px",
-  },
   wrapper: {
+    backgroundImage: "url('./login-background-image.avif')",
+    backgroundSize: "cover",
+    height: "100%",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignContent: "center",
-    height: "90%",
-    width: "25%",
-    margin: "0 auto",
-    textAlign: "center",
-    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
-      width: "80%",
-      justifyContent: "start",
-      marginTop: "8%",
-    },
+  },
+  loginWrapper: {
+    backgroundColor: "white,",
+    padding: "3rem",
+    zIndex: 999,
   },
 }));
 
