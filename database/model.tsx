e515b36c84ccfe8f8a27a57493967e9c1cfca8f3
miahbates <miahbates@icloud.com>
2022-10-08 12:38:28 +0100

    fix(auth) check if user in db and redirect to home if there or create new user in db

--- database/model.tsx
+++ database/model.tsx
@@ -3,7 +3,16 @@ import db from "../database/connection";
 export const createUser = (email: string) => {
   const INSERT_USER = `INSERT INTO users(email) VALUES($1)`;
   return db.query(INSERT_USER, [email]).then((result: any) => {
-    console.log(result, "result");
     return result.rows[0];
+  });
+};
+
+export const searchDbUsers = (email: string) => {
+  const LOOK_UP_USER = `SELECT * FROM users WHERE email = $1`;
+  return db.query(LOOK_UP_USER, [email]).then((result: any) => {
+    if (result.rows.length === 1) {
+      return result.rows[0];
+    }
+    return null;
   });
 };
--- src/pages/api/login.tsx
+++ src/pages/api/login.tsx
@@ -1,13 +1,18 @@
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
-import { createUser } from "../../../database/model";
+import { createUser, searchDbUsers } from "../../../database/model";
 
 export default withApiAuthRequired(async function handler(req, res) {
   const session = getSession(req, res);
-  console.log(session?.user, "session");
-  console.log("1 $$$$$$$$");
+
   if (session?.user) {
-    const query = await createUser(session.user.email);
-    console.log(query, "query");
+    const searchDbUsersQuery = await searchDbUsers(session?.user.email);
+    if (searchDbUsersQuery) {
+      console.log(searchDbUsersQuery, "searchDbUsersQuery");
+      res.redirect(301, "/home");
+    } else {
+      await createUser(session.user.email);
+      res.redirect(301, "/home");
+    }
   }
 
   res.redirect(301, "/home");
